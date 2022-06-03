4a2f55fd8dd990285098bfd2acb7c78d988fc019
cristi <cristian.mardare@gmail.com>
2022-06-03 16:04:14 +0300

    Change Person to record

--- src/main/java/homework/logicalSwitch/LogicalSwitch.java
+++ src/main/java/homework/logicalSwitch/LogicalSwitch.java
@@ -3,7 +3,6 @@ package homework.logicalSwitch;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class LogicalSwitch {
@@ -11,20 +10,20 @@ public class LogicalSwitch {
 //
     public LogicalSwitch() {
         this.map = new HashMap<>();
-        NamedPredicate<Person> p1 = new NamedPredicate<>(p ->p.getName().startsWith("M"),"StartsWith M Filter");
-        NamedPredicate<Person> p2 = new NamedPredicate<>(p ->p.getAge() < 10,"Young Filter");
-        NamedPredicate<Person> p3 = new NamedPredicate<>(p ->p.getAge() > 60,"Old Filter");
-        NamedPredicate<Person> p4 = new NamedPredicate<>(p ->p.getAddress().length() > 10,"Address Filter");
-        this.map.put(p1, (p) -> "%s starts with M".formatted(p.getName()));
-        this.map.put(p2, (p) -> "%s is young".formatted(p.getName()));
-        this.map.put(p3, (p) -> "%s is old".formatted(p.getName()));
-        this.map.put(p4, (p) -> "%s's address too long, replaced with %s...".formatted(p.getName(),p.getAddress().substring(0,10)));
+        NamedPredicate<Person> p1 = new NamedPredicate<>(p ->p.name().startsWith("M"),"StartsWith M Filter");
+        NamedPredicate<Person> p2 = new NamedPredicate<>(p ->p.age() < 10,"Young Filter");
+        NamedPredicate<Person> p3 = new NamedPredicate<>(p ->p.age() > 60,"Old Filter");
+        NamedPredicate<Person> p4 = new NamedPredicate<>(p ->p.address().length() > 10,"Address Filter");
+        this.map.put(p1, (p) -> "%s starts with M".formatted(p.name()));
+        this.map.put(p2, (p) -> "%s is young".formatted(p.name()));
+        this.map.put(p3, (p) -> "%s is old".formatted(p.name()));
+        this.map.put(p4, (p) -> "%s's address too long, replaced with %s...".formatted(p.name(),p.address().substring(0,10)));
     }
 
     public String compute(Person p){
         return map.entrySet()
                 .stream()
-                .map(m -> m.getKey().predicate().test(p)?m.getValue().apply(p):"%s not applied for %s".formatted(m.getKey().name(),p.getName()))
+                .map(m -> m.getKey().predicate().test(p)?m.getValue().apply(p):"%s not applied for %s".formatted(m.getKey().name(),p.name()))
                 .collect(Collectors.joining("\n"));
     }
 
--- src/main/java/homework/logicalSwitch/Person.java
+++ src/main/java/homework/logicalSwitch/Person.java
@@ -1,26 +1,5 @@
 package homework.logicalSwitch;
 
-public class Person {
-    private final String name;
-    private final int age;
-    private final String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public Person(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+public record Person(String name, int age, String address) {
 
 }
